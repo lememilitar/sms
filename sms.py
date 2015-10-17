05c23c2084a402eca020320b64ad93d462b5dea5
Caio Moura <caiomoura1994@gmail.com>
2015-10-17 11:28:55 -0300

    Pimeiro commit

---
+++ sms.py
@@ -0,0 +1,32 @@
+import serial
+import time
+
+class TextMessage:
+    def __init__(self):
+        pass
+
+    def setRecipient(self, number):
+        self.recipient = number
+
+    def setContent(self, message):
+        self.content = message
+
+    def connectPhone(self):
+        self.ser = serial.Serial('/dev/ttyUSB0', 460800, timeout=5, xonxoff = False, rtscts = False, bytesize = serial.EIGHTBITS, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE)
+        time.sleep(1)
+
+    def sendMessage(self):
+        self.ser.write('ATZ\r')
+        time.sleep(1)
+        self.ser.write('AT+CMGF=1\r')
+        time.sleep(1)
+        self.ser.write('''AT+CMGS="''' + self.recipient + '''"\r''')
+        time.sleep(1)
+        self.ser.write(self.content + "\r")
+        time.sleep(1)
+        self.ser.write(chr(26))
+        time.sleep(1)
+        return self.ser.readline()
+
+    def disconnectPhone(self):
+        self.ser.close()
